42507b99e9403a99bbc38bc47ab3eb5974413a7d
Ciarra <ciarra1040@gmail.com>
2023-08-10 11:43:22 -0400

    Add javascript

--- Code/Language/HTML/js/main.js
+++ Code/Language/HTML/js/main.js
@@ -1,2 +1,6 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+person = {name:"John", age:31, city:"New York"};
+const myObj = {name:"John", age:30, city:"New York"};
+document.getElementById("person").innerHTML = myObj.name;
